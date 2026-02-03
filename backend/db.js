e6efbab501368fe7f4487c5eb01df2155817904e
Aryan Shrivastav <aryanshrivastav4370@gmail.com>
2026-02-03 21:04:37 +0530

    Skilldash V.0.1.1

--- backend/db.js
+++ backend/db.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 
 // Create the connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  port: Number(process.env.DB_PORT) || 3306,
-  password: process.env.DB_PASSWORD || '',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  port: Number(process.env.DB_PORT) ,
+  password: process.env.DB_PASSWORD ,
   ssl:{
     rejectUnauthorized:true
   },
-  database: process.env.DB_NAME || 'skilldash_db',
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
