2cb6e2ec7b711a9b1a9d6699ec358f1f2e524db7
Aryan Shrivastav <aryanshrivastav4370@gmail.com>
2026-02-04 14:01:35 +0530

    Skilldash V.0.1.1

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
