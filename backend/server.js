8a3248ce76d26c3d823938fce85b187378f96cdf
Aryan Shrivastav <aryanshrivastav4370@gmail.com>
2026-02-04 12:46:43 +0530

    Skilldash V.0.1.1

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,14 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP CONNECTED");
+  }
 });
 
 // 3. SOCKET.IO
